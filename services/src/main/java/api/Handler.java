9bb18444cfe5d26963445f60a166ee0df77db512
Driss Amri <drissamri+git@gmail.com>
2022-10-10 11:50:52 +0200

    Init SST

---
+++ services/src/main/java/api/Handler.java
@@ -0,0 +1,16 @@
+package api;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>{
+  @Override
+  public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+    APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+    response.setStatusCode(200);
+    response.setBody("Hello, Driss! Received a request with id " + event.getRequestContext().getRequestId());
+    return response;
+  }
+}
